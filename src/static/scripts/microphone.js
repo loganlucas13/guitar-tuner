0dcfc93c87487656f6d7b6c875cf46342ca82653
Logan Lucas <logs10658@gmail.com>
2024-11-24 17:34:27 -0600

    refactored getMicrophonePermissions and added comments

--- src/static/scripts/microphone.js
+++ src/static/scripts/microphone.js
@@ -1,3 +1,4 @@
+// adds all required event listeners after page has fully loaded
 document.addEventListener("DOMContentLoaded", () => {
     const enableMicrophoneButton = document.getElementById('enable-microphone');
     enableMicrophoneButton.addEventListener('click', () => {
@@ -5,18 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// helper function for 'enable-button' event handler
+// asks for access to user's microphone (popup in top-left)
 function getMicrophonePermissions(enableMicrophoneButton) {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({audio: true})
-        .then(function(stream) {
-            console.log('microphone enabled successfully!');
-            enableMicrophoneButton.style.display = 'none';
-        })
-        .catch(function(error) {
-            console.error('microphone not enabled:', error);
-        });
+    // aborts if browser does not support getUserMedia
+    if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+        console.error('ERROR: getUserMedia not allowed');
+        return;
     }
-    else {
-        console.error('getUserMedia not allowed');
-    }
+
+    navigator.mediaDevices.getUserMedia({audio: true})
+    .then(function(stream) { // success
+        console.log('Microphone enabled successfully!');
+        enableMicrophoneButton.style.display = 'none'; // hide button
+    })
+    .catch(function(error) { // failure
+        console.error('ERROR: Microphone not enabled -', error);
+    });
 }
